13373a98003bafc18d13635b74fe86da9d00aff9
Augusto <augusto.ferrari@joinsolutions.com.ar>
2015-11-23 21:33:59 -0300

    Agregado de Carpeta public

---
+++ public/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $(".getCharacters").click(function() {
+
+    $.get("/characters", function(data) {
+      data = JSON.parse(data);
+      data.results.forEach(function(character, index) {
+        var listItem = $('<li></li>');
+        var info = "" +
+          "name: " + character.name +
+          "height: " + character.height +
+          "mass: " + character.mass +
+          "hair color: " + character.hair_color +
+          "skin color: " + character.skin_color +
+          "eye color: " + character.eye_color +
+          "birth year: " + character.birth_year +
+          "gender: " + character.gender;
+        listItem.text(info);
+
+        listItem.addClass('collection-item');
+
+        $(".character-list").empty();
+        $(".character-list").append(listItem);
+      });
+    });
+
+  });
+});
